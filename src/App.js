f7cbb2229c7d5c7b6a08f32faa04aecb7275ab24
Tripp Lucas <tripppdx@gmail.com>
2021-12-13 19:48:05 -0800

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/index.js';
 import Header from './components/Header';
@@ -7,16 +8,28 @@ import Footer from './components/Footer';
 import Categories from './components/Categories';
 import Products from './components/Products';
 import SimpleCart from './components/SimpleCart';
+import ShoppingCart from './components/ShoppingCart';
 
 function App() {
   return (
     <>
       <Provider store={store()}>
-        <Header />
-        <Categories />
-        <Products />
-        <SimpleCart />
-        <Footer />
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Header />
+              <Categories />
+              <Products />
+              <SimpleCart />
+              <Footer />
+            </Route>
+            <Route exact path="/cart">
+              <Header />
+              <ShoppingCart />
+              <Footer />
+            </Route>
+          </Switch>
+        </Router>
       </Provider>
     </>
   );
